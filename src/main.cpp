57e37eeda39139b2e9f40e8d66c8b88e8850bb80
helloimglace <someoneplzhelpmecuzimcrazynow@gmail.com>
2025-03-20 12:52:23 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include "matrix.h"
+
+int main() {
+    int n;
+    float a[MAX][MAX], inv[MAX][MAX];
+
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            std::cin >> a[i][j];
+        }
+    }
+
+    if (inverse(a, inv, n)) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%6.2f", inv[i][j]);
+            }
+            std::cout << std::endl;
+        }
+    } else {
+        std::cout << "No solution" << std::endl;
+    }
+}
